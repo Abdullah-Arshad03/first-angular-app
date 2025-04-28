ee9e28087efa72250e4e076138570fe5edd6280c
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2025-04-28 23:46:05 +0500

    learned attribute binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -9,6 +9,7 @@ import { CourseService } from "./courses.service";
 })
 export class CoursesComponent{
     title = 'list of courses';
+    imageUrl = 'https://images.unsplash.com/photo-1745794621090-d856c53b0cc2?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
     getTitle(){
     return  this.title
     }
