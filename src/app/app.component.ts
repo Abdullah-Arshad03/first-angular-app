f414b5c22431a28f846b3e04a29f939b7476c888
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2025-04-29 21:20:30 +0500

    two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,15 @@ import { HeaderComponent } from './header.component';
 import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
 import { CourseService } from './courses.service';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ CoursesComponent, CourseComponent ],
+  imports: [ CoursesComponent, CourseComponent],
   providers:[CourseService],
   templateUrl: './app.component.html',
-  // styleUrl: './app.component.css'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'first-angular-app ';
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,21 +1,52 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { CourseService } from "./courses.service";
+// import { CourseService } from "./courses.service";
+import { FormsModule, NgModel } from "@angular/forms";
 
 @Component({
-    selector : 'courses', 
-    templateUrl :'./courses.component.html',
-    imports : [CommonModule]
+    selector: 'courses', 
+    templateUrl: './courses.component.html',
+    imports: [CommonModule, FormsModule],
 })
-export class CoursesComponent{
-    title = 'list of courses';
-    imageUrl = 'https://images.unsplash.com/photo-1745794621090-d856c53b0cc2?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
-    getTitle(){
-    return  this.title
-    }
-    courses;
-    constructor(public service : CourseService){
-        this.courses = service.getCourses()
-    };
+export class CoursesComponent {
 
+     
+    isActive = true
+    xyz = 'yellow'
+
+    email = 'imabdullahhhh@gmail.com'
+
+    enteredd(){
+        console.log('this is email now :', this.email)
+    }
+    divClicked(){
+        console.log('div is clicked!')
+    }
+    entered($event:Event){
+        const target = $event.target as HTMLInputElement
+        this.email = target.value
+        console.log('entered bro', this.email)
+    }
+    onClick($event:Event){
+        $event.stopPropagation()
+        console.log("im clicked!", $event)
+    }
+    entering(kisib : string){
+        console.log('just made : ', kisib)
+    }
+    // onpressingEnter($event : Event) {
+    //     const target = $event.target as HTMLInputElement; // Type assertion
+    //     console.log('i pressed',target.value); // Access the value correctly
+    // }
+
+
+    // title = 'list of courses';
+    // imageUrl = 'https://images.unsplash.com/photo-1745794621090-d856c53b0cc2?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+    // getTitle(){
+    // return  this.title
+    // }
+    // courses;
+    // constructor(public service : CourseService){
+    //     this.courses = service.getCourses()
+    // };
 }
